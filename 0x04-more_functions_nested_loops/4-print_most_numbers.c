e909ec02fdccf3be796a390daa7ec59392d3887c
Lordsoye121 <lordsoye121@gmail.com>
2022-09-16 17:16:05 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,13 +12,12 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
+	{
+
+		if ((n == 50) || (n == 52))
 		{
-
-			if ((n == 50) || (n == 52))
-			{
-				continue;															}
-			putchar(n);
-		}
-
+			continue;															}
+		putchar(n);
+	}
 	putchar(10);
 }
